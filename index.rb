ba673621333ec8d605039d59002e0f98546f33c8
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-29 06:09:17 +1000

    Initial import

---
+++ index.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+get '/api/quote' do
+  authors = File.readlines('authors.txt')
+  quotes = File.readlines('quotes.txt')
+  randomIndex = rand(authors.length)
+
+  "{\"quote\": \"" + quotes[randomIndex] + "\", " +
+      "\"author\": \"" + authors[randomIndex] + "\", " +
+      "\"appVersion\": \"1.0.0\", " +
+      "\"environmentName\": \"" + ENV["ENVIRONMENT_NAME"] ||= "Development" + "\" " +
+      "}";
+end
