425fec5fda24ca845eed6dc256fc707cbffa8f13
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-29 07:19:38 +1000

    Added static app

--- lib/index.rb
+++ lib/index.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 
 get '/api/quote' do
-  authors = File.readlines('authors.txt')
-  quotes = File.readlines('quotes.txt')
+  authors = File.readlines('authors.txt').each {|l| l.chomp!}
+  quotes = File.readlines('quotes.txt').each {|l| l.chomp!}
   randomIndex = rand(authors.length)
 
-  "{\"quote\": \"" + quotes[randomIndex] + "\", " +
-      "\"author\": \"" + authors[randomIndex] + "\", " +
-      "\"appVersion\": \"1.0.0\", " +
-      "\"environmentName\": \"" + ENV["ENVIRONMENT_NAME"] ||= "Development" + "\" " +
-      "}"
+  <<EOF
+  {
+    "quote": "#{quotes[randomIndex]}",
+    "author": "#{authors[randomIndex]}",
+    "appVersion": "1.0.0",
+    "environmentName": "#{ENV["ENVIRONMENT_NAME"] ||= "Development"}"
+  }
+EOF
 end
